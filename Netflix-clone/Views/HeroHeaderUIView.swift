5349f55295cf2293e4c81a8cf853efaf861053e4
furkanyilmazPetroo <furkan.yilmaz@anttechbva.com>
2023-12-14 16:07:41 +0300

    logo and the buttons are added

--- Netflix-clone/Views/HeroHeaderUIView.swift
+++ Netflix-clone/Views/HeroHeaderUIView.swift
@@ -9,6 +9,26 @@ import UIKit
 
 class HeroHeaderUIView: UIView {
     
+    private let downloadButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Download", for: .normal)
+        button.layer.borderColor = UIColor.red.cgColor
+        button.layer.borderWidth = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
+    private let playButtpn: UIButton = {
+        let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.layer.borderColor = UIColor.red.cgColor
+        button.layer.borderWidth = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
     private let heroImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
@@ -27,10 +47,30 @@ class HeroHeaderUIView: UIView {
         layer.addSublayer(gradientLayer)
     }
     
+    private func applyConstraints() {
+        let playButtonConstraints = [
+            playButtpn.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 70),
+            playButtpn.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            playButtpn.widthAnchor.constraint(equalToConstant: 120)
+        ]
+        
+        let downloadButtonConstraints = [
+            downloadButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -70),
+            downloadButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            downloadButton.widthAnchor.constraint(equalToConstant: 120)
+        ]
+        
+        NSLayoutConstraint.activate(playButtonConstraints)
+        NSLayoutConstraint.activate(downloadButtonConstraints)
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(heroImageView)
         addGradient()
+        addSubview(playButtpn)
+        addSubview(downloadButton)
+        applyConstraints()
     }
     
     override func layoutSubviews() {
