e09337bfdba76b0e64b0b5904760afd63c3c4d44
furkanyilmazPetroo <furkan.yilmaz@anttechbva.com>
2023-12-14 11:29:26 +0300

    Add the controllers to the main controller

---
+++ Netflix-clone/Controllers/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  Netflix-clone
+//
+//  Created by Furkan Yılmaz on 14.12.2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+}
