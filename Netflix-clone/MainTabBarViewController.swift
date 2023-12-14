84b6fe42d029a125db180e129744cb5ab1e76741
furkanyilmazPetroo <furkan.yilmaz@anttechbva.com>
2023-12-14 11:40:32 +0300

    Changing the tab colors and giving them images

--- Netflix-clone/MainTabBarViewController.swift
+++ Netflix-clone/MainTabBarViewController.swift
@@ -11,11 +11,25 @@ class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemYellow    }
-
-    let vc1 = UINavigationController(rootViewController: HomeViewController())
-    let vc2 = UINavigationController(rootViewController: UpcomingViewController())
-    let vc3 = UINavigationController(rootViewController: SearchViewController())
-    let vc4 = UINavigationController(rootViewController: DownloadsViewController())
+        view.backgroundColor = .systemYellow
+        
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        let vc2 = UINavigationController(rootViewController: UpcomingViewController())
+        let vc3 = UINavigationController(rootViewController: SearchViewController())
+        let vc4 = UINavigationController(rootViewController: DownloadsViewController())
+        
+        vc1.tabBarItem.image = UIImage(systemName: "house")
+        vc2.tabBarItem.image = UIImage(systemName: "play.circle")
+        vc3.tabBarItem.image = UIImage(systemName: "mignifyingglass")
+        vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        
+        vc1.title = "Home"
+        vc2.title = "Coming"
+        vc3.title = "Top Search"
+        vc4.title = "Downloads"
+        
+        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
+        
+    }
 }
 
