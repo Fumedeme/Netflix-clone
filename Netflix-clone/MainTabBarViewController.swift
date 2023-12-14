35b53fdd0c0ba1c6a8aea4f2e5954e41183bf3d3
furkanyilmazPetroo <furkan.yilmaz@anttechbva.com>
2023-12-14 11:42:14 +0300

    Changing the color

--- Netflix-clone/MainTabBarViewController.swift
+++ Netflix-clone/MainTabBarViewController.swift
@@ -20,13 +20,15 @@ class MainTabBarViewController: UITabBarController {
         
         vc1.tabBarItem.image = UIImage(systemName: "house")
         vc2.tabBarItem.image = UIImage(systemName: "play.circle")
-        vc3.tabBarItem.image = UIImage(systemName: "mignifyingglass")
+        vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
         vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
         
         vc1.title = "Home"
         vc2.title = "Coming"
         vc3.title = "Top Search"
         vc4.title = "Downloads"
+        
+        tabBar.tintColor = .label
         
         setViewControllers([vc1, vc2, vc3, vc4], animated: true)
         
