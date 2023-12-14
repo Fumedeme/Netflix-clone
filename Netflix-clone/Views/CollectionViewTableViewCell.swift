afebbb291ebf321cdab2f37ec0c5f95822e4d011
furkanyilmazPetroo <furkan.yilmaz@anttechbva.com>
2023-12-14 14:07:36 +0300

    Table View cell is added and included in the main tab

---
+++ Netflix-clone/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Netflix-clone
+//
+//  Created by Furkan Yılmaz on 14.12.2023.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+    static let identifier = "CollectionViewTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemPink
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError()
+    }
+}
